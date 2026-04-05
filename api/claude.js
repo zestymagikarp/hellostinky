dcb1728f1f853e28b448d9cae547fb21038bd3ce
zestymagikarp <carr.lucas11@gmail.com>
2026-04-05 16:48:21 -0500

    claude?

--- api/claude.js
+++ api/claude.js
@@ -3,7 +3,9 @@ export const config = {
     bodyParser: {
       sizeLimit: '25mb',
     },
+    responseLimit: false,
   },
+  maxDuration: 60,
 }
 
 export default async function handler(req, res) {
